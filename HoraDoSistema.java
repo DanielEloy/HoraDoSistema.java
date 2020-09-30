13ab730e0c9e9be932c2f55c2240adc206e7605f
DanielEloy <58008758+DanielEloy@users.noreply.github.com>
2020-09-30 00:13:35 -0300

    Update and rename README.md to HoraDoSistema.java

---
+++ HoraDoSistema.java
@@ -0,0 +1,16 @@
+# HoraDoSistema.javas
+/**
+ * @author Daniel Eloy
+ */
+package HoraDoSistema;
+
+import java.util.Date;
+
+	public class HoraDaSistema {
+		
+		public static void main (String[] args)  {
+			Date relogio = new Date();
+			System.out.println("A hora do sistema");
+			System.out.println(relogio.toString());
+		}
+	}
